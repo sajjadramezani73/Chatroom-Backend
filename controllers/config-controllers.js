e6030e54f596659c1edabbcaa1e2ae4a2a0bdf1a
sajjadramezani <sajjad.payam77@gmail.com>
2022-10-18 22:39:24 +0330

    fix: Update config controllers

--- controllers/config-controllers.js
+++ controllers/config-controllers.js
@@ -1,24 +1,24 @@
 const jwt = require('jsonwebtoken')
+const User = require('../model/user')
 
 const getIndex = async (req, res, next) => {
     const reqToken = req.headers.authorization
 
-    let userValid
     if (reqToken == undefined) {
-        userValid = false
+        res.json({ userValid: false, user: null })
     } else {
         const token = reqToken.split(' ')[1]
-        const validToken = jwt.verify(token, 'secret_key', (err, decoded) => {
+        const validToken = jwt.verify(token, 'secret_key', async (err, decoded) => {
             if (err) {
-                userValid = false
+                res.json({ userValid: false, user: null })
             }
             else {
-                userValid = true
+                const user = await User.findOne({ _id: decoded.userId })
+                res.json({ userValid: true, user: user })
             }
             next()
         })
     }
-    res.json({ userValid: userValid })
 
 }
 
